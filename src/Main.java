ea6857d9b34296bafec88f8b26407028c3b23ddf
Michael Kelly <cybernationnews@gmail.com>
2022-09-13 22:15:50 -0500

    Adding new main file

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        Client testClient = new Client();
+//        testClient.startServer("localhost", 1234);
+//        System.out.println("Client Started\n");
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        while (true) {
+//            String userInput = scanner.nextLine();
+//            System.out.println(testClient.request(userInput));
+//
+//            if (userInput.equals("q")) {
+//                break;
+//            }
+//        }
+    }
+}
